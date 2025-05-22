b15b34681c56f69762646d501b8f205328691e71
keysithoru <kustoroterbatas@gmail.com>
2025-05-22 14:44:59 +0700

    revisi perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1,2));
 console.log("ini test saja");
 const deduct = (a, b) => a -b;
 
-const mutiply = (a, b) => a * b;
-console.log(mutiply(2,2));
+const mutiply_number = (a, b) => a * b;
+console.log(mutiply_number(2,2));
 
 const divide = (a, b) => a * b;
 console.log(divide(2,2));
